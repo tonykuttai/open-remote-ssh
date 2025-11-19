7a01fcd8c39068482637dc418c0d5012f7a3f30d
Tony Varghese <tonypalampalliyil@gmail.com>
2025-11-20 03:52:53 +0530

    Bump version to 0.0.52

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
-npm install -g ovsx 
-npx ovsx create-namespace tonykuttai -p  ovsxp_a3e6ad0a-4128-4662-baf5-6f56734e73a5
-bash build_and_install.sh
+# npm install -g ovsx 
+# npx ovsx create-namespace tonykuttai -p  ovsxp_a3e6ad0a-4128-4662-baf5-6f56734e73a5
+# bash build_and_install.sh
+yarn compile
+npx vsce package --yarn
+
 npx ovsx publish -p  ovsxp_a3e6ad0a-4128-4662-baf5-6f56734e73a5
