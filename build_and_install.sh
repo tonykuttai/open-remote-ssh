82efbd5ad6f17815ff1166e180918908cc7d4ebb
Tony Varghese <tonypalampalliyil@gmail.com>
2025-11-23 10:50:21 +0530

    update publish.sh and build script

--- build_and_install.sh
+++ build_and_install.sh
@@ -4,4 +4,4 @@ npx vsce package
 # codium --install-extension open-remote-ssh-0.0.50.vsix
 # codium-insiders --install-extension open-remote-ssh-0.0.50.vsix
 
-codium --install-extension aix-remote-ssh-0.0.50.vsix
+# codium --install-extension aix-remote-ssh-0.0.50.vsix
--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
+# To publish a new release, run the following commands:
+# Modify the package.json to update the version
+# Set the GITHUB_TOKEN environment variable with your GitHub token
+# Then run this script 
 yarn compile
 npx vsce package --yarn
 
